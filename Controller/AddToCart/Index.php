6126251381a31f56d40692d99879b942da29e36a
Joris Wagter <joris@webwijs.nu>
2024-06-25 10:34:04 +0200

    made add_to_cart item array same as view_cart

--- Controller/AddToCart/Index.php
+++ Controller/AddToCart/Index.php
@@ -2,6 +2,7 @@
 
 namespace Taggrs\DataLayer\Controller\AddToCart;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
@@ -19,18 +20,22 @@ class Index extends AbstractDataLayerController
 
     private QuoteDataHelper $quoteDataHelper;
 
+    private ProductRepositoryInterface $productRepository;
+
     public function __construct(
         Context $context,
         JsonFactory $resultJsonFactory,
         Session $checkoutSession,
         UserDataHelper $userDataHelper,
         StoreManagerInterface $storeManager,
-        QuoteDataHelper $quoteDataHelper
+        QuoteDataHelper $quoteDataHelper,
+        ProductRepositoryInterface $productRepository
     ) {
         parent::__construct($context, $resultJsonFactory, $userDataHelper, $storeManager);
 
         $this->checkoutSession = $checkoutSession;
         $this->quoteDataHelper = $quoteDataHelper;
+        $this->productRepository = $productRepository;
     }
 
     public function getEvent(): string
@@ -41,11 +46,53 @@ class Index extends AbstractDataLayerController
     public function getEcommerce(): array
     {
 
+        try {
+            $quote = $this->checkoutSession->getQuote();
+        } catch (NoSuchEntityException|LocalizedException $e) {
+        }
+
+        if (!isset($quote)) {
+            return [];
+        }
+
+        $max = 0;
+        $lastItem = null;
+
+        foreach ($quote->getAllVisibleItems() as $quoteItem) {
+            if ($quoteItem->getId() > $max) {
+                $max = $quoteItem->getId();
+                $lastItem = $quoteItem;
+            }
+        }
+
+        if ($lastItem === null) {
+            return [];
+        }
+
+        if ($lastItem->getProduct()->getTypeId() === 'configurable') {
+            $configProduct = $this->productRepository->getById($lastItem->getProduct()->getId());
+            $item['item_id'] = $configProduct->getSku();
+            $item['item_variant'] = $lastItem->getSku();
+        } else {
+            $item['item_id'] = $lastItem->getSku();
+        }
+
+        $item['item_name'] = $lastItem->getName();
+        $item['price'] = (float)$lastItem->getPriceInclTax();
+        $item['quantity'] = $lastItem->getQty();
+
+        if ($lastItem->getDiscountAmount() > 0) {
+            $item['discount'] = $lastItem->getDiscountAmount();
+        }
+
+        if ($lastItem->getQuote()->getCouponCode()) {
+            $item['coupon'] = $lastItem->getQuote()->getCouponCode();
+        }
+
+        $item = array_merge($item, $this->quoteDataHelper->getCategoryNamesByProduct($lastItem->getProduct()));
+
         return [
-            'currency' => $this->getCurrency(),
-            'value' => (float)$this->checkoutSession->getQuote()->getGrandTotal(),
-            'items' => $this->quoteDataHelper->getItemsFromQuote(),
-            'user_data' => $this->getUserData(),
+            'items' => [$item]
         ];
     }
 }
